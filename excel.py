f6b047fea44434413bf2ec3b094da58cb001e280
Stalin <stalinrodriguez775@gmail.com>
2026-03-02 13:35:49 +0100

    aplicando control de errores

--- excel.py
+++ excel.py
@@ -1,7 +1,15 @@
 import pandas as pd
-def guardar_excel(tabla_inventario, nombre_archivo="Inventario.xlsx"):
+def guardar_excel(tabla_inventario):
     try:
-        tabla_inventario.to_excel(nombre_archivo, index=False) #index borra la columna extra para que quede mas limpio
-        print(f"Archivo {nombre_archivo} guardado correctamente.")
+        # Validación de robustez (Punto 3 de la rúbrica)
+        if tabla_inventario["codigo"].duplicated().any():
+            print("Existen códigos duplicados.")
+            return
+
+        tabla_inventario.to_excel("Inventario.xlsx", index=False)#index borra la columna extra para que quede mas limpio
+        print("Archivo guardado.")
+
+    except PermissionError:
+        print(f"El archivo 'Inventario.xlsx' está abierto. Ciérralo e intenta de nuevo.")
     except Exception as e:
-        print(f"Hubo un error al guardar: {e}")
+        print(f"Error no se puede añadir: {e}")
